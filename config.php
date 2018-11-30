e121a9475086fd837684fafe22caa40510b30cb1
DV <firstsquares@gmail.com>
2018-11-30 10:07:24 +0100

    Update config.php
    Removed database details

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'C1RT4Nmysql');
-define('DB_NAME', 'google');
+define('DB_PASS', 'password');
+define('DB_NAME', 'dbname');
 
 //define("ROOT_PATH", "OOP-project/");
-define("ROOT_URL", "/~deepavarma/WebsiteBuilder/OOP-project/");
+define("ROOT_URL", "/OOP-project/");
 ?>
